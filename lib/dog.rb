fc76257eb658e142c7f9590501b78f97d721d41d
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-27 16:28:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # dog.rb
+class Dog
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+spot = Dog.new
+spot.name = "Spot"
+spot.breed = "Dachshund"
+puts spot.name
+puts spot.breed
